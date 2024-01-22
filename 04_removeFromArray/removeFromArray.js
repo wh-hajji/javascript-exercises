760c661547112c1d23acbfbdd128b292f1aace88
wajih <wajih@wajih-unix>
2024-01-22 17:35:50 +0100

    feat: Exercice Done!
    	splice(index, number of cases);
    	for inside for; arguments of functions.
    array methodes still need more education.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...myArg) {
+    for (let i = 0; i < myArg.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if ( myArg[i] === arr[j]){
+                arr.splice(j,1);
+                j--;
+            }
+            
+        }
+        
+    }
+    return arr;
 };
 
 // Do not edit below this line
