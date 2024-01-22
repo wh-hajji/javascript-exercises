e748aa508eeba2e6fff05de6684fcf3811a60e3e
wajih <wajih@wajih-unix>
2024-01-22 18:17:15 +0100

    feat: Exercice Done!
    	conditions6+loop for, nothing special

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (typeof(a) !== 'number' || typeof(b) !== 'number' || a<0 || b<0 ) {
+        return 'ERROR';        
+    }else{
+        let min=a;
+        let max=b;
+        let sum=0;
+        if(a>b){min=b;max=a}
+        
+        for (let i = min; i <= max; i++) {
+            sum+=i        
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
